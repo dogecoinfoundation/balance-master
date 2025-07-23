05ea18f2951a9343dfbd572109926874a024e6f6
Daniel Whelan <chobomuffin@gmail.com>
2025-07-23 08:52:55 +0100

    testing

--- cmd/balance-master/main.go
+++ cmd/balance-master/main.go
@@ -14,7 +14,7 @@ func main() {
 	var dogeSchema, dogeHost, dogePort, dogeUser, dogePassword, databaseURL, migrationsPath, rpcServerHost, rpcServerPort string
 
 	flag.StringVar(&dogeSchema, "doge-schema", "http", "Dogecoin schema")
-	flag.StringVar(&dogeHost, "doge-host", "localhost", "Dogecoin host")
+	flag.StringVar(&dogeHost, "doge-host", "dogecoin", "Dogecoin host")
 	flag.StringVar(&dogePort, "doge-port", "22556", "Dogecoin port")
 	flag.StringVar(&dogeUser, "doge-user", "test", "Dogecoin user")
 	flag.StringVar(&dogePassword, "doge-password", "test", "Dogecoin password")
